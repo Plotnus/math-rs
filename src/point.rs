3c8c769051e58d45211ca41d17427d0f052d8a5d
plot <plotnus@gmail.com>
2019-12-05 00:33:39 -0500

    add some const fn

--- src/point.rs
+++ src/point.rs
@@ -13,8 +13,20 @@ struct Point {
 impl Point {
     pub const ZERO: Point = Point { x: 0.0, y: 0.0, z: 0.0 };
     pub const ONES: Point = Point { x: 1.0, y: 1.0, z: 1.0 };
-    pub fn new(x: f32, y: f32, z: f32) -> Point {
+    pub const fn new(x: f32, y: f32, z: f32) -> Point {
         Point {x, y, z}
+    }
+
+    pub const fn uniform(u: f32) -> Point {
+        Point::new(u,u,u)
+    }
+
+    pub const fn zero() -> Point {
+        Point::uniform(0.0)
+    }
+
+    pub const fn ones() -> Point {
+        Point::uniform(1.0)
     }
 }
 
@@ -47,12 +59,12 @@ mod tests {
 
     #[test]
     fn add_vec3d() {
-        assert_eq!(Point::new(2.0, 2.0, 2.0), Point::ONES + Vec3::ONES);
+        assert_eq!(Point::new(2.0, 2.0, 2.0), Point::ones() + Vec3::ones());
     }
     #[test]
-    fn point3d_add_ref_vec3d() {
+    fn add_ref_vec3d() {
         let v = Vec3::new(1.0,1.0,1.0);
-        assert_eq!(Point::new(2.0, 2.0, 2.0), Point::ONES + &v);
+        assert_eq!(Point::new(2.0, 2.0, 2.0), Point::ones() + &v);
 
         println!("{:?}", v);
     }
--- src/vector.rs
+++ src/vector.rs
@@ -1,8 +1,4 @@
-struct _Vec2d {
-    x: f32,
-    y: f32,
-}
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Vec3 {
     pub x: f32,
     pub y: f32,
@@ -10,11 +6,76 @@ pub struct Vec3 {
 }
 
 impl Vec3 {
-    pub const ONES: Vec3 = Vec3 {x: 1.0, y: 1.0, z: 1.0 };
-    pub const _ZERO: Vec3 = Vec3 {x: 0.0, y: 0.0, z: 0.0 };
-
-    pub fn new(x: f32, y: f32, z: f32) -> Vec3 {
+    pub const fn new(x: f32, y: f32, z: f32) -> Vec3 {
         Vec3 { x, y, z}
+    }
+
+    pub const fn uniform(u: f32) -> Vec3 {
+        Vec3::new(u,u,u)
+    }
+
+    pub const fn ones() -> Vec3 {
+        Vec3::uniform(1.0)
+    }
+
+    pub const fn zero() -> Vec3 {
+        Vec3::uniform(0.0)
+    }
+
+    pub const fn i_hat() -> Vec3 {
+        Vec3::new(1.0, 0.0, 0.0)
+    }
+
+    pub const fn j_hat() -> Vec3 {
+        Vec3::new(0.0, 1.0, 0.0)
+    }
+
+    pub const fn k_hat() -> Vec3 {
+        Vec3::new(0.0, 0.0, 1.0)
     }
 }
 
+impl core::ops::Mul<f32> for Vec3 {
+    type Output = Vec3;
+    fn mul(self, s: f32) -> Vec3 {
+        Vec3 {
+            x: self.x * s,
+            y: self.y * s,
+            z: self.z * s,
+        }
+    }
+}
+
+impl core::ops::Sub<Vec3> for Vec3 {
+    type Output = Vec3;
+    fn sub(self, rhs: Vec3) -> Vec3 {
+        Vec3 {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z,
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::vector::Vec3;
+    #[test]
+    fn mul_f32() {
+        let expected = Vec3::new(0.0, 0.0, 0.0);
+        let result = Vec3::ones() * 0.0;
+        assert_eq!(expected, result);
+    }
+
+    // something to notice about the following test that can be viewed as bad is
+    // that the test is not independent of other fun
+    #[test]
+    fn sub_vec3() {
+        assert_eq!(
+            Vec3::new(1.0, 0.0, -1.0),
+            Vec3::new(100.0, 50.0, 25.0) - Vec3::new(99.0, 50.0, 26.0)
+        );
+    }
+
+}
